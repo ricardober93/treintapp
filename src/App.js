16d5aed04b281e35248f767aff4e40c8fc00729e
ricardober93 <ricardobzorro@gmail.com>
2020-10-10 19:20:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Route } from "wouter";
+import { routes } from 'routes';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      TreintaApp
+      {
+        routes.map(({ path, component }) => (
+          <Route path={path} component={component } />
+        ))
+       }
     </div>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Login from 'components/Login/index'
+import Register from 'components/Register/index'
+export const routes = [{
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/register',
+        component: Register
+    }
+]
